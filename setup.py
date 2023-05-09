8a82e0203e262da3214ec42233968c68ba5f6905
Developer Junio <developer@junio.in>
2023-05-09 12:57:39 +0530

    [feature][writer] es threaded batch writer

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='elasticsearch-indexer',
     packages=['elasticsearch_indexer'],
-    version='0.0.1',
+    version='0.0.2',
     author="Develper Junio",
     author_email='developer@junio.in',
     classifiers=[
