a2c31cfaea2438a92a908b8d5e1fb4331f07535d
Developer Junio <developer@junio.in>
2023-05-09 12:56:47 +0530

    [feature][writer] es threaded batch writer

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='elasticsearch-indexer',
+    packages=['elasticsearch_indexer'],
+    version='0.0.1',
+    author="Develper Junio",
+    author_email='developer@junio.in',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="Thread based bulk writer for elasticsearch",
+    license="MIT license",
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "elasticsearch == 7.14.0",
+        "structlog == 23.1.0"
+    ]
+)
